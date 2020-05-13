c7540298c1e03739020226c42c8bb505c417e2f9
IvanTS1991 <ivanadh1991@gmail.com>
2020-05-13 14:49:27 +0200

    Sistemas/

---
+++ Test.java
@@ -0,0 +1,16 @@
+/*
+	Archivo de prueba que mostrara primero un mensaje y luego modificaremos mediante consola para que aparezcan dos mensajes
+ **/
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+
+
+	System.out.println("Hola Mundo");
+
+	}
+
+
+
+}
